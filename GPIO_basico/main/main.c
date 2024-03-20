4596497a9cb6182f081633a4e94079d69aa5e96e
andrevale99 <andrefvale@gmail.com>
2024-03-20 08:26:59 -0300

    Correção para acionar o LED apenas quando soltar o botão

--- GPIO_basico/main/main.c
+++ GPIO_basico/main/main.c
@@ -16,29 +16,28 @@
 
 static const char *LED_TAG = "LEDS";
 
-void app_main() {
-  
+void app_main()
+{
+
   esp_log_level_set("*", ESP_LOG_INFO);
 
   gpio_config_t ledsConfig =
-  {
-    .pin_bit_mask = (1<<LED_R) | (1<<LED_G) | (1<<LED_Y),
-    .mode = GPIO_MODE_OUTPUT,
-    .pull_up_en = GPIO_PULLUP_DISABLE,
-    .pull_down_en = GPIO_PULLDOWN_DISABLE,
-    .intr_type = GPIO_INTR_DISABLE
-  };
+      {
+          .pin_bit_mask = (1 << LED_R) | (1 << LED_G) | (1 << LED_Y),
+          .mode = GPIO_MODE_INPUT_OUTPUT,
+          .pull_up_en = GPIO_PULLUP_DISABLE,
+          .pull_down_en = GPIO_PULLDOWN_DISABLE,
+          .intr_type = GPIO_INTR_DISABLE};
 
   ESP_ERROR_CHECK(gpio_config(&ledsConfig));
 
   gpio_config_t botoesConfig =
-  {
-    .pin_bit_mask = (1<<BT_1) | (1<<BT_2),
-    .mode = GPIO_MODE_INPUT,
-    .pull_up_en = GPIO_PULLUP_ENABLE,
-    .pull_down_en = GPIO_PULLDOWN_DISABLE,
-    .intr_type = GPIO_INTR_DISABLE
-  };
+      {
+          .pin_bit_mask = (1 << BT_1) | (1 << BT_2),
+          .mode = GPIO_MODE_INPUT,
+          .pull_up_en = GPIO_PULLUP_ENABLE,
+          .pull_down_en = GPIO_PULLDOWN_DISABLE,
+          .intr_type = GPIO_INTR_DISABLE};
 
   ESP_ERROR_CHECK(gpio_config(&botoesConfig));
 
@@ -47,21 +46,36 @@ void app_main() {
   gpio_set_level(LED_Y, 0);
 
   int status_led = 0;
+  int previous_state = (1 << LED_R) | (1 << LED_G);
 
-  while (true) 
+  while (true)
   {
-    status_led = gpio_get_level(LED_R);  
+    status_led = gpio_get_level(LED_R);
     ESP_LOGI(LED_TAG, "LED_R: %d", status_led);
     status_led = gpio_get_level(LED_G);
     ESP_LOGI(LED_TAG, "LED_G: %d", status_led);
     status_led = gpio_get_level(LED_Y);
     ESP_LOGI(LED_TAG, "LED_Y: %d \n", status_led);
 
-    if(!gpio_get_level(BT_1))
-      gpio_set_level(LED_R, !gpio_get_level(LED_R));
+    ESP_LOGI("STATE", "%d", previous_state);
+    ESP_LOGI("BT_1", "%d", gpio_get_level(BT_1));
+    ESP_LOGI("BT_2", "%d \n", gpio_get_level(BT_2));
 
-    if(!gpio_get_level(BT_2))
+    if (!gpio_get_level(BT_1) && (previous_state & (1 << LED_R)))
+    {
+      previous_state &= ~(1 << LED_R);
+      gpio_set_level(LED_R, !gpio_get_level(LED_R));
+    }
+    else if (gpio_get_level(BT_1) && !(previous_state & (1 << LED_R)))
+      previous_state |= (1 << LED_R);
+
+    if (!gpio_get_level(BT_2) && (previous_state & (1 << LED_G)))
+    {
+      previous_state &= ~(1 << LED_G);
       gpio_set_level(LED_G, !gpio_get_level(LED_G));
+    }
+    else if (gpio_get_level(BT_2) && !(previous_state & (1 << LED_G)))
+      previous_state |= (1 << LED_G);
 
     gpio_set_level(LED_Y, !gpio_get_level(LED_Y));
 
