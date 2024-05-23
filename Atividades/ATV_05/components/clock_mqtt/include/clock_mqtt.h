976da47bbe7a2db95be295aa0b42923e7d2946cc
andrevale99 <andrefvale@gmail.com>
2024-05-22 23:12:55 -0300

    Delete e Rename

--- Atividades/ATV_05/components/clock_mqtt/include/clock_mqtt.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLOCK_MQTT_H
-#define CLOCK_MQTT_H
-
-void clockMQTT_config(void);
-
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
-
-#endif
